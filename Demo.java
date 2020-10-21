5959f8683a0621f8a9abc9195946f38dbd4b83c9
jzhowie <jzhou20@stuy.edu>
2020-10-21 17:20:32 -0400

    Added arrayDeepToString method

--- Demo.java
+++ Demo.java
@@ -1,12 +1,24 @@
 public class Demo {
 public static void main(String[] args) {
-	int[] x1 = {1,2,3,34};
-	int[] x2 = {1,2};
-	int[] x3 = {1,5,15,334,17,33};
-	int[] x4 = {4};
-	int[] x5 = {1,2,3,16,27};
-	System.out.println(arrToString(x2));
+	if (args.length == 0) {
+		printLoop(5);
+	}
+	else {
+		printLoop(Integer.parseInt(args[0]));
+	}
+	/*
+	   int[] x1 = {1,2,3,34};
+	   int[] x2 = {1,2};
+	   int[] x3 = {1,5,15,334,17,33};
+	   int[] x4 = {4};
+	   int[] x5 = {1,2,3,16,27};
+	   int[][] x6 = {{1,2,3,16,27},{1,5,15,334,17,33}};
+	   System.out.println(arrToString(x2));
+	   System.out.println(arrayDeepToString(x6));
+	 */
 }
+
+
 
 public static String arrToString(int[] arr) {
 	String arrstr = "{";
@@ -20,7 +32,17 @@ public static String arrToString(int[] arr) {
 	return arrstr;
 }
 
-
+public static String arrayDeepToString(int[][] arr) {
+	String newstr = "{";
+	for (int row = 0; row < arr.length; row++) {
+		newstr += arrToString(arr[row]);
+		if (row != arr.length - 1) {
+			newstr += ", ";
+		}
+	}
+	newstr += "}";
+	return newstr;
+}
 
 public static void printLoop(int a) {
 	for (int x = 1; x <= a; x++) {
